6da7fe4b3fc1a149793dfcbb94fc3c181cd4be63
BrownEdgar <nikogosjanedgar@gmail.com>
2022-02-15 20:44:56 +0400

    add work 2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Edgar/homeworks/work1/App'
+import App from './components/Edgar/homeworks/work2/App'
 
 ReactDOM.render(<App />,document.getElementById('root'));
 
